54853e4beb5993bad129582c76b511a91748070e
HuyNT <tienhuylionel@gmail.com>
2024-09-24 13:36:12 +0700

    update code

--- section_001/create-projects.bash
+++ section_001/create-projects.bash
@@ -9,8 +9,8 @@ spring init \
 --java-version=17 \
 --packaging=jar \
 --name=product-service \
---package-name=se.magnus.microservices.core.product \
---groupId=se.magnus.microservices.core.product \
+--package-name=hnt.microservices.core.product \
+--groupId=hnt.microservices.core.product \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 product-service
@@ -21,8 +21,8 @@ spring init \
 --java-version=17 \
 --packaging=jar \
 --name=review-service \
---package-name=se.magnus.microservices.core.review \
---groupId=se.magnus.microservices.core.review \
+--package-name=hnt.microservices.core.review \
+--groupId=hnt.microservices.core.review \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 review-service
@@ -33,8 +33,8 @@ spring init \
 --java-version=17 \
 --packaging=jar \
 --name=recommendation-service \
---package-name=se.magnus.microservices.core.recommendation \
---groupId=se.magnus.microservices.core.recommendation \
+--package-name=hnt.microservices.core.recommendation \
+--groupId=hnt.microservices.core.recommendation \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 recommendation-service
@@ -45,8 +45,8 @@ spring init \
 --java-version=17 \
 --packaging=jar \
 --name=product-composite-service \
---package-name=se.magnus.microservices.composite.product \
---groupId=se.magnus.microservices.composite.product \
+--package-name=hnt.microservices.composite.product \
+--groupId=hnt.microservices.composite.product \
 --dependencies=actuator,webflux \
 --version=1.0.0-SNAPSHOT \
 product-composite-service
